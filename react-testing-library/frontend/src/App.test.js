b6f41958a6f1bc7f900eeed388e8d697318c432c
VicenteObando <juan.obando.a@tecsup.edu.pe>
2021-05-19 21:22:31 -0500

    Test en React

--- react-testing-library/frontend/src/App.test.js
+++ react-testing-library/frontend/src/App.test.js
@@ -1,8 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react'
+import '@testing-library/jest-dom/extend-expect'
+import { render } from '@testing-library/react'
+import { fireEvent, prettyDOM } from '@testing-library/dom'
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test('renders content', ()=> {
+  const app = {
+    content: 'Phonebook',
+    important: true
+  }
+
+  const component = render(<App app={app}/>)
+  component.getByText('Phonebook')
+  component.getByText('filter show with')
+
+})
